7ea2203c7af45b1c8bf9e390ee09ca59ea59ec0e
nityaaj <nyjardosh@gmail.com>
2025-12-26 19:15:53 +0530

    refactor: remove scroll indicator from HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -150,22 +150,6 @@ export default function HomePage() {
 						</div>
 					</motion.div>
 				</div>
-
-				{/* Scroll indicator */}
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ delay: 1, duration: 0.5 }}
-					className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-				>
-					<div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
-						<motion.div
-							animate={{ y: [0, 12, 0] }}
-							transition={{ repeat: Infinity, duration: 1.5 }}
-							className="w-1 h-3 bg-white rounded-full mt-2"
-						/>
-					</div>
-				</motion.div>
 			</section>
 
 			{/* Trust Pillars Section */}
